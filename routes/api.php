f311c3b8fa88091633ad9711417c94fbe824c49e
janckos <janckos@gmail.com>
2023-10-31 17:13:13 -0600

    Adding src

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/tasks', [\App\Http\Controllers\Api\TaskController::class, 'index']);
+Route::post('/tasks', [\App\Http\Controllers\Api\TaskController::class, 'store']);
+Route::get('/tasks/{id}', [\App\Http\Controllers\Api\TaskController::class, 'show']);
+Route::put('/tasks/{id}', [\App\Http\Controllers\Api\TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [\App\Http\Controllers\Api\TaskController::class, 'destroy']);
